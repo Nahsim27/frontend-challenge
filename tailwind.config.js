1ddd6451eb50801d40b0aa7d18b259a33040e355
Mishan <mishan@Saybhans-Air.lan>
2022-03-02 13:50:06 +0000

    Added more styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,35 @@
 module.exports = {
     content: ["./*.{html,js}"],
     theme: {
+          container: {
+              center: true,
+              padding: {
+                DEFAULT: '2rem',
+                sm: '5rem',
+              },
+          },
       extend: {},
     },
     plugins: [],
   }
+//   theme: {
+//     container: {
+//         center: true,
+//         padding: {
+//           DEFAULT: '2rem',
+//           sm: '5rem',
+//         },
+//     },
+//     fontFamily: {
+//         sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+//         heading: ['"Red Hat Display"', ...defaultTheme.fontFamily.sans],
+//     },
+//     extend: {
+//         colors: {
+//             greyblack: '#0D0D0D',
+//             oriongreen: '#05BE68',
+//             grey: '#80828D',
+//             lightergrey: '#1E1E1E',
+//         },
+//     },
+// },
